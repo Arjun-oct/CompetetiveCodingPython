59edc0e00c4449ff0c3eb434bde0a90b79f61e50
Arjun-oct <77836429+Arjun-oct@users.noreply.github.com>
2021-01-22 18:08:54 +0700

    Update shortestpath.py

--- shortestpath.py
+++ shortestpath.py
@@ -7,20 +7,17 @@ def shortestPath(graph,src,dest):
     # keep a track record of vertices with the cost
     # heappop will return vertex with least cost
     # greedy SRC -> MIN - > MIN -> MIN -> DEST
-
-    heapq.heappush(h,(0,src))
-   
+    
+     heapq.heappush(h,(0,src))
+  
     while len(h)!=0:
         currcost,currvtx = heapq.heappop(h)
         if currvtx == dest:
             print("Path Exisits {} to {} with cost {}".format(src,dest,currcost))
             break
-        for neigh,neighcost in graph[currvtx]:
+        for neigh,neighcost in graph[currvtx] :
             heapq.heappush(h,(currcost+neighcost,neigh))
-            
         
-    
-    
 graph = defaultdict(list)
 v,e = map(int,input().split())
 for i in range(e):
